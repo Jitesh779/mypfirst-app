97fe725f15c39421378b3382585f54cdc36c5f19
Jitesh779 <you@example.com>
2023-08-28 23:57:13 +0530

    first comment

---
+++ list.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class list {
+    public static void main(String[] args) {
+        List list = new ArrayList<>(0);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        System.out.println(list.get(2));
+
+    }
+}
